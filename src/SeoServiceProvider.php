de2d2f5bffee266faf2791aacec583a2598a7221
David Rushton <david@papertank.co.uk>
2020-03-28 08:37:59 +0000

    Dropped support for Laravel 5.

--- src/SeoServiceProvider.php
+++ src/SeoServiceProvider.php
@@ -4,54 +4,54 @@ namespace Origami\Seo;
 
 use Illuminate\Support\ServiceProvider;
 
-class SeoServiceProvider extends ServiceProvider {
+class SeoServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/seo.php' => config_path('seo.php'),
+        ]);
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->publishes([
-			__DIR__.'/config/seo.php' => config_path('seo.php'),
-		]);
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->mergeConfigFrom(
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
             __DIR__ . '/config/seo.php',
             'seo'
         );
 
-		$this->app->bind('origami.seo', function($app)
-        {
+        $this->app->bind(Seo::class, function ($app) {
             $config = $app['config']->get('seo', []);
             return new Seo($config);
         });
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['origami.seo'];
-	}
+        $this->app->alias(Seo::class, 'origami.seo');
+    }
 
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['origami.seo'];
+    }
 }
